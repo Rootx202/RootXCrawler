05ec775a23e03b1debf7f2aafb7c654e0b0a3b2a
rootx <learnwithrootx@gmail.com>
2026-06-02 01:59:50 +0300

    Add error handling to external API plugins

--- plugins/dnsdumpster.py
+++ plugins/dnsdumpster.py
@@ -1,22 +1,31 @@
 """Support for dnsdumpster.com."""
 import re
+import sys
 
 import requests
 
 
 def dnsdumpster(domain, output_dir):
     """Query dnsdumpster.com."""
-    response = requests.Session().get('https://dnsdumpster.com/').text
-    csrf_token = re.search(
-        r'name=\"csrfmiddlewaretoken\" value=\"(.*?)\"', response).group(1)
+    session = requests.Session()
+    try:
+        response = session.get('https://dnsdumpster.com/', timeout=30)
+        csrf_match = re.search(
+            r'name=\"csrfmiddlewaretoken\" value=\"(.*?)\"', response.text)
+        if not csrf_match:
+            print("[-] Could not extract CSRF token from dnsdumpster.com", file=sys.stderr)
+            return
+        csrf_token = csrf_match.group(1)
 
-    cookies = {'csrftoken': csrf_token}
-    headers = {'Referer': 'https://dnsdumpster.com/'}
-    data = {'csrfmiddlewaretoken': csrf_token, 'targetip': domain}
-    response = requests.Session().post(
-        'https://dnsdumpster.com/', cookies=cookies, data=data, headers=headers)
+        cookies = {'csrftoken': csrf_token}
+        headers = {'Referer': 'https://dnsdumpster.com/'}
+        data = {'csrfmiddlewaretoken': csrf_token, 'targetip': domain}
+        session.post(
+            'https://dnsdumpster.com/', cookies=cookies, data=data, headers=headers, timeout=30)
 
-    image = requests.get('https://dnsdumpster.com/static/map/%s.png' % domain)
-    if image.status_code == 200:
-        with open('%s/%s.png' % (output_dir, domain), 'wb') as f:
-            f.write(image.content)
+        image = session.get('https://dnsdumpster.com/static/map/%s.png' % domain, timeout=30)
+        if image.status_code == 200:
+            with open('%s/%s.png' % (output_dir, domain), 'wb') as f:
+                f.write(image.content)
+    except Exception as e:
+        print("[-] Failed to query dnsdumpster.com: %s" % e, file=sys.stderr)
--- plugins/find_subdomains.py
+++ plugins/find_subdomains.py
@@ -1,14 +1,18 @@
 """Support for findsubdomains.com."""
+import sys
 from re import findall
 
-from requests import get
+from requests import get, ConnectionError, Timeout
 
 
 def find_subdomains(domain):
     """Find subdomains according to the TLD."""
     result = set()
-    response = get('https://findsubdomains.com/subdomains-of/' + domain).text
-    matches = findall(r'(?s)<div class="domains js-domain-name">(.*?)</div>', response)
-    for match in matches:
-        result.add(match.replace(' ', '').replace('\n', ''))
+    try:
+        response = get('https://findsubdomains.com/subdomains-of/' + domain, timeout=30)
+        matches = findall(r'(?s)<div class="domains js-domain-name">(.*?)</div>', response.text)
+        for match in matches:
+            result.add(match.replace(' ', '').replace('\n', ''))
+    except (ConnectionError, Timeout) as e:
+        print("[-] Failed to fetch subdomains from findsubdomains.com: %s" % e, file=sys.stderr)
     return list(result)
